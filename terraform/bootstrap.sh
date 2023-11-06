78f65c5e2081b570b5a4d734c9da64af2bf0eb02
Michael Jones <25686778+mikeacjones@users.noreply.github.com>
2023-11-06 12:38:04 -0500

    Added in the monthly lock of previous posts

--- terraform/bootstrap.sh
+++ terraform/bootstrap.sh
@@ -30,7 +30,7 @@ for subreddit_name in $secret_names; do
     --restart on-failure:5 \
     trade-confirmation-bot
 
-  # Create the service file
+  # Create the service file for creating the monthly post
   echo "[Unit]" >>/etc/systemd/system/$subreddit_name-monthly-post.service
   echo "Description=Creates monthly post for r/$subreddit_name" >>/etc/systemd/system/$subreddit_name-monthly-post.service
   echo "" >>/etc/systemd/system/$subreddit_name-monthly-post.service
@@ -38,7 +38,7 @@ for subreddit_name in $secret_names; do
   echo "Type=oneshot" >>/etc/systemd/system/$subreddit_name-monthly-post.service
   echo "ExecStart=docker exec $subreddit_name python3 bot.py create-monthly" >>/etc/systemd/system/$subreddit_name-monthly-post.service
 
-  # Create the timer file
+  # Create the timer file for creating the monthly post
   echo "[Unit]" >>/etc/systemd/system/$subreddit_name-monthly-post.timer
   echo "Description=Trigger for monthly post for r/$subreddit_name" >>/etc/systemd/system/$subreddit_name-monthly-post.timer
   echo "" >>/etc/systemd/system/$subreddit_name-monthly-post.timer
@@ -48,6 +48,25 @@ for subreddit_name in $secret_names; do
   echo "" >>/etc/systemd/system/$subreddit_name-monthly-post.timer
   echo "[Install]" >>/etc/systemd/system/$subreddit_name-monthly-post.timer
   echo "WantedBy=timers.target" >>/etc/systemd/system/$subreddit_name-monthly-post.timer
+
+  # Create the service file for locking previous months
+  echo "[Unit]" >>/etc/systemd/system/$subreddit_name-lock-post.service
+  echo "Description=Locks old posts for r/$subreddit_name" >>/etc/systemd/system/$subreddit_name-lock-post.service
+  echo "" >>/etc/systemd/system/$subreddit_name-lock-post.service
+  echo "[Service]" >>/etc/systemd/system/$subreddit_name-lock-post.service
+  echo "Type=oneshot" >>/etc/systemd/system/$subreddit_name-lock-post.service
+  echo "ExecStart=docker exec $subreddit_name python3 bot.py lock-submissions" >>/etc/systemd/system/$subreddit_name-lock-post.service
+
+  # Create the timer file for locking previous months
+  echo "[Unit]" >>/etc/systemd/system/$subreddit_name-lock-post.timer
+  echo "Description=Locks old posts for r/$subreddit_name" >>/etc/systemd/system/$subreddit_name-lock-post.timer
+  echo "" >>/etc/systemd/system/$subreddit_name-lock-post.timer
+  echo "[Timer]" >>/etc/systemd/system/$subreddit_name-lock-post.timer
+  echo "OnCalendar=*-*-05 00:00:00" >>/etc/systemd/system/$subreddit_name-lock-post.timer
+  echo "Persistent=true" >>/etc/systemd/system/$subreddit_name-lock-post.timer
+  echo "" >>/etc/systemd/system/$subreddit_name-lock-post.timer
+  echo "[Install]" >>/etc/systemd/system/$subreddit_name-lock-post.timer
+  echo "WantedBy=timers.target" >>/etc/systemd/system/$subreddit_name-lock-post.timer
 
   systemctl daemon-reload
   systemctl enable $subreddit_name-monthly-post.timer
